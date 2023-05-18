2ff1d848d7aad72c5cf77e24340f611848c8dfdf
Sooki <0xSooki@gmail.com>
2023-05-18 20:55:40 +0200

    fix paths

--- api/utils/checks.py
+++ api/utils/checks.py
@@ -1,5 +1,4 @@
 from soup import *
-from difflib import SequenceMatcher
 import Levenshtein
 
 
@@ -20,17 +19,6 @@ def find_similar_strings(target, string_list):
         return 0
     else:
         return None
-
-
-# Example usage
-target_string = "apple"
-string_list = ["appple", "app", "kiwi", "ple", "mango"]
-similar_string = find_similar_strings(target_string, string_list)
-
-if similar_string is not None:
-    print(f"A similar string to '{target_string}' is '{similar_string}'.")
-else:
-    print(f"No similar string found for '{target_string}'.")
 
 
 def capsCheckH(allCaps, wordCount):
@@ -68,6 +56,3 @@ def domainCheck(url):
         if j == dom[1]:
             l2 = 0
     return (l1+l2)/2
-
-
-print(domainCheck(url))
