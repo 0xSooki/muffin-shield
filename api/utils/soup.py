3ba335fcfa88faf7f5f51154e07402c67163faf8
JIUHWA <67797035+IIT25@users.noreply.github.com>
2023-05-18 16:55:31 +0200

    article-body

--- api/utils/soup.py
+++ api/utils/soup.py
@@ -10,8 +10,9 @@ def extract_content(url):
 
     if h1:
         print(h1.text)
+    content = soup.find('article',{'class':'article-body'})
+    print(content.text)
 
-
-url = ""
+url = "https://www.origo.hu/tudomany/20230517-nem-ajanlja-a-szintetikus-es-a-termeszetes-edesitoszerek-hosszu-tavu-hasznalatat-a-who.html"
 
 extract_content(url)
