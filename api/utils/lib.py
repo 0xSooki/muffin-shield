750bc800f80eb6462ea5719ea05f106c1493ae6c
JIUHWA <67797035+IIT25@users.noreply.github.com>
2023-05-18 17:31:17 +0200

    capsCheck works

--- api/utils/lib.py
+++ api/utils/lib.py
@@ -1,2 +1,22 @@
+from soup import extract_content
+
 def test() -> str:
     return "Hello, world!"
+
+def capsCheckH(allCaps, wordCount):
+    if (allCaps/wordCount > 0.7):       
+        return 1
+    else:
+        return 0
+    
+def capsCheck(content):
+    conList = content.split()
+    filter(None, conList)
+    words = len(conList)
+    c = 0
+    for word in conList:
+        if(word.isupper()):
+            c += 1
+    return (capsCheckH(c,words))
+url= "https://www.origo.hu/tudomany/20230517-nem-ajanlja-a-szintetikus-es-a-termeszetes-edesitoszerek-hosszu-tavu-hasznalatat-a-who.html"
+print(capsCheck(extract_content(url)))
--- api/utils/soup.py
+++ api/utils/soup.py
@@ -8,10 +8,8 @@ def extract_content(url):
 
     h1 = soup.find('h1')
 
-    if h1:
-        print(h1.text)
     content = soup.find('article',{'class':'article-body'})
-    print(content.text)
+    return content.text
 
 url = "https://www.origo.hu/tudomany/20230517-nem-ajanlja-a-szintetikus-es-a-termeszetes-edesitoszerek-hosszu-tavu-hasznalatat-a-who.html"
 
